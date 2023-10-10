e32bc8354218911411b2b5e25d8fafc993732abf
swantrace <frederickhong.5@gmail.com>
2023-10-10 09:02:38 -0700

    add an example test

---
+++ src/tests/example.test.jsx
@@ -0,0 +1,9 @@
+// example test
+import { describe, it, expect } from "vitest";
+
+describe("example", () => {
+  it("should be true", () => {
+    const sum = 1 + 1;
+    expect(sum).toBe(2);
+  });
+});
